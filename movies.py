70a932b05a6fe636da5cd212db1b80e5f241802a
Gaby <gaby_dva_@hotmail.com>
2020-01-18 11:19:28 -0600

    Added jwt validation to all routes

--- movies.py
+++ movies.py
@@ -6,7 +6,7 @@ import sqlite3,json
 import logging
 from logging.handlers import RotatingFileHandler
 from flask_jwt_extended import (
-    JWTManager, jwt_required, create_access_token,
+    JWTManager, jwt_required, jwt_optional,create_access_token,
     jwt_refresh_token_required, create_refresh_token,
     get_jwt_identity, set_access_cookies,
     set_refresh_cookies, unset_jwt_cookies
@@ -19,7 +19,10 @@ app = Flask(__name__)
 app.config['JWT_TOKEN_LOCATION'] = ['cookies']
 app.config['JWT_ACCESS_COOKIE_PATH'] = ['/movie/list','/api/example','/movie/add/rent']
 app.config['JWT_SECRET_KEY'] = 'movie-test' 
-app.config['JWT_CSRF_CHECK_FORM '] = True
+#app.config['JWT_CSRF_CHECK_FORM '] = True
+app.config['JWT_COOKIE_CSRF_PROTECT'] = False
+#https://flask-jwt-extended.readthedocs.io/en/latest/tokens_in_cookies/
+#https://flask-jwt-extended.readthedocs.io/en/stable/options/#cross-site-request-forgery-options
 
 jwt = JWTManager(app)
 
@@ -34,29 +37,7 @@ handler.setFormatter(formatter)
 logger.addHandler(console)
 logger.addHandler(handler)
 
-# @app.route('/token/auth', methods=['POST'])
-# def logintest():
-    # username = str(request.form['user'])
-    # password = str(request.form['pass'])
-    # if username != 'test' or password != 'test':
-        # return jsonify({'login': False}), 401
 
-    # # Create the tokens we will be sending back to the user
-    # access_token = create_access_token(identity=username)
-
-    # # Set the JWT cookies in the response
-    # resp = jsonify({'login': True})
-    # set_access_cookies(resp, access_token)
-
-    # return resp, 200
-	
-# @app.route('/api/example', methods=['GET'])
-# @jwt_required
-# def protected():
-	# username = get_jwt_identity()
-	# return jsonify({'hello': 'from {}'.format(username)}), 200
-	
-	
 #TODOS
 #Welcome page, lists basic movie info
 @app.route('/')
@@ -74,7 +55,6 @@ def movies_list():
 	except Exception as e:
 		print(e)
 		return jsonify(status="error")
-
 
 
 #ADMIN
@@ -131,7 +111,7 @@ def rent_movie():
 		current_user_name = get_jwt_identity()
 		if current_user_name:
 			role=check_role(current_user_name)
-			if 'user' in role:
+			if 'client' in role:
 				try:
 					id_user = str(request.form['id_user'])
 					id_movie=str(request.form['id_movie'])
@@ -140,92 +120,116 @@ def rent_movie():
 					conn = sqlite3.connect("movies.db")
 					conn.execute("INSERT INTO rents(id_user,id_movie,return_date,price_penalty) VALUES (?,?,?,?)",(id_user,id_movie,return_date,price_penalty));
 					conn.commit()
-					logger.info("The user "+str(id_user)+" has rented the movie "+str(id_movie))
+					logger.info("The user "+str(current_user_name)+" has rented the movie "+str(id_movie))
 					return jsonify(status="success",current_user=current_user_name,rol=role), 201
 				except Exception as e:
 					print(e)
 					return jsonify(data="error"),
 				conn.close()
+			return jsonify(status="error, you're not allowed",current_user=current_user_name,rol=role), 200
 
 
 #USER
 @app.route('/movie/add/buy', methods=['POST'])
+@jwt_required
 def buy_movie():
 	if request.method == 'POST':
-		try:
-			id_user = str(request.form['id_user'])
-			id_movie=str(request.form['id_movie'])
-			stock=0
-			conn = sqlite3.connect("movies.db")
-			cursor = conn.cursor()
-			cursor.execute("select stock from movies where id_movie="+str(id_movie))
-			for record in cursor.fetchall():
-				stock=record[0]
-			if stock is 0:
-				return jsonify(status="error, no stock"), 200
-			else:
-				#update stock
-				stock-=1
-				cursor = conn.cursor()
-				cursor.execute("UPDATE movies SET stock=? WHERE id_movie=?",(stock,str(id_movie)))
-				conn.execute("INSERT INTO sales(id_user,id_movie) VALUES (?,?)",(id_user,id_movie));
-				conn.commit()
-				logger.info("The user "+str(id_user)+" has bought the movie "+str(id_movie))
-				return jsonify(status="success"), 201
-		except Exception as e:
-			print(e)
-			return jsonify(data="error"),
-		conn.close()
-	
-#USER	
+		current_user_name = get_jwt_identity()
+		if current_user_name:
+			role=check_role(current_user_name)
+			if 'client' in role:
+				try:
+					id_user = str(request.form['id_user'])
+					id_movie=str(request.form['id_movie'])
+					stock=0
+					conn = sqlite3.connect("movies.db")
+					cursor = conn.cursor()
+					cursor.execute("select stock from movies where id_movie="+str(id_movie))
+					for record in cursor.fetchall():
+						stock=record[0]
+					if stock is 0:
+						return jsonify(status="error, no stock"), 200
+					else:
+						#update stock
+						stock-=1
+						cursor = conn.cursor()
+						cursor.execute("UPDATE movies SET stock=? WHERE id_movie=?",(stock,str(id_movie)))
+						conn.execute("INSERT INTO sales(id_user,id_movie) VALUES (?,?)",(id_user,id_movie));
+						conn.commit()
+						logger.info("The user "+str(current_user_name)+" has bought the movie "+str(id_movie))
+						return jsonify(status="success",current_user=current_user_name,rol=role), 201
+				except Exception as e:
+					print(e)
+					return jsonify(data="error"),
+				conn.close()
+			else:	
+				return jsonify(status="error, you're not allowed",current_user=current_user_name,rol=role), 200	
+
+
+#USER
 @app.route('/movie/<int:key>/like', methods=['PUT'])
+@jwt_required
 def like_movie(key):
 	if request.method == 'PUT':
-		try:
-			#select value
-			num_likes=[]
-			conn = sqlite3.connect("movies.db")
-			cursor = conn.cursor()
-			cursor.execute("select popularity from movies where id_movie="+str(key))
-			for record in cursor.fetchall():
-				num_likes=record[0]
-			num_likes+=1
-			#insert new value
-			cursor = conn.cursor()
-			cursor.execute("UPDATE movies SET popularity=? WHERE id_movie=?",(num_likes,str(key)))
-			conn.commit()
-			logger.info("Movie "+str(key)+" has been liked")
-			return jsonify(status="success",data=num_likes), 200
-		except Exception as e:
-			print(e)
-			return jsonify(status="error"),
-		conn.close()
+		current_user_name = get_jwt_identity()
+		if current_user_name:
+			role=check_role(current_user_name)
+			if 'client' in role:
+				try:
+					#select value
+					num_likes=[]
+					conn = sqlite3.connect("movies.db")
+					cursor = conn.cursor()
+					cursor.execute("select popularity from movies where id_movie="+str(key))
+					for record in cursor.fetchall():
+						num_likes=record[0]
+					num_likes+=1
+					#insert new value
+					cursor = conn.cursor()
+					cursor.execute("UPDATE movies SET popularity=? WHERE id_movie=?",(num_likes,str(key)))
+					conn.commit()
+					logger.info("Movie "+str(key)+" has been liked")
+					return jsonify(status="success",data=num_likes,current_user=current_user_name,rol=role), 200
+				except Exception as e:
+					print(e)
+					return jsonify(status="error"),
+				conn.close()
+			else:	
+				return jsonify(status="error, you're not allowed",current_user=current_user_name,rol=role), 200
 		
-		
-#SOLO ADMIN		
+
+#SOLO ADMIN
 @app.route('/movie/add', methods=['POST'])
+@jwt_required
 def movies_add():
 	if request.method == 'POST':
-		try:
-			title = str(request.form['title'])
-			desc=str(request.form['desc'])
-			stock=str(request.form['stock'])
-			image=str(request.form['image'])
-			rent_price=float(request.form['rent_price'])
-			sale_price=float(request.form['sale_price'])
-			availability=int(request.form['availability'])
-			popularity=int(request.form['popularity'])
-			conn = sqlite3.connect("movies.db")
-			conn.execute("INSERT INTO movies(title,description,image,stock,rent_price,sale_price,availability,popularity) VALUES (?,?,?,?,?,?,?,?)",(title,desc,image,stock,rent_price,sale_price,availability,popularity));
-			conn.commit()
-			return jsonify(status="success"), 201
-		except Exception as e:
-			print(e)
-			return jsonify(data="error"),
-		conn.close()		
+		current_user_name = get_jwt_identity()
+		if current_user_name:
+			role=check_role(current_user_name)
+			if 'admin' in role:
+				try:
+					title = str(request.form['title'])
+					desc=str(request.form['desc'])
+					stock=str(request.form['stock'])
+					image=str(request.form['image'])
+					rent_price=float(request.form['rent_price'])
+					sale_price=float(request.form['sale_price'])
+					availability=int(request.form['availability'])
+					popularity=int(request.form['popularity'])
+					conn = sqlite3.connect("movies.db")
+					conn.execute("INSERT INTO movies(title,description,image,stock,rent_price,sale_price,availability,popularity) VALUES (?,?,?,?,?,?,?,?)",(title,desc,image,stock,rent_price,sale_price,availability,popularity));
+					conn.commit()
+					conn.close()
+					return jsonify(status="success",current_user=current_user_name,rol=role), 201
+				except Exception as e:
+					print(e)
+					return jsonify(data="error"),
+			else:	
+				return jsonify(status="error, you're not allowed",current_user=current_user_name,rol=role), 200
 		
-		
-@app.route('/movie/<int:key>/', methods=['GET','PUT', 'DELETE'],)
+
+@app.route('/movie/<int:key>/', methods=['GET','PUT', 'DELETE'])
+@jwt_optional
 def movies_detail(key):
 	#TODOS
 	if request.method == 'GET':
@@ -236,53 +240,58 @@ def movies_detail(key):
 			cursor.execute("select * from movies where id_movie="+str(key))
 			for record in cursor.fetchall():
 				movie.append({'title': str(record[1]), 'desc': str(record[2]), 'img':str(record[3])})
-
+			cursor.close()
+			conn.close()
 			return jsonify(status="success",data=movie), 201
 		except Exception as e:
 			print(e)
 			return jsonify(status="error"),
-		cursor.close()
-		conn.close()
-	
-	#ADMIN
-	if request.method == 'PUT':
-		try:
-			title = str(request.form['title'])
-			desc=str(request.form['desc'])
-			stock=str(request.form['stock'])
-			desc=str(request.form['desc'])
-			image=str(request.form['image'])
-			rent_price=float(request.form['rent_price'])
-			sale_price=float(request.form['sale_price'])
-			availability=int(request.form['availability'])
-			popularity=int(request.form['popularity'])
-			#insert de nuevos valores
-			conn = sqlite3.connect("movies.db")
-			cursor = conn.cursor()
-			cursor.execute("UPDATE movies SET title=?, description=?,image=?,stock=?, rent_price=?,sale_price=?, availability=?, popularity=? WHERE id_movie=?",(title,desc,image,stock,rent_price,sale_price,availability,popularity,str(key)))
-			conn.commit()
-			logger.info(str(title)+" has been updated, rental price: $"+str(rent_price)+", sale price: $"+str(rent_price))
-			return jsonify(status="success"), 200
-		except Exception as e:
-			print(e)
-			return jsonify(status="error"),
-		conn.close()
-		
-	#ADMIN
-	if request.method == 'DELETE':
-		try:
-			movie=[]
-			conn = sqlite3.connect("movies.db")
-			conn.execute("DELETE FROM movies WHERE id_movie="+str(key))
-			conn.commit()
-			conn.close()
 
-			return jsonify(status="success"), 201
-		except Exception as e:
-			print(e)
-			return jsonify(data="error"),
-		cursor.close()
-		conn.close()
+	else:
+		current_user_name = get_jwt_identity()
+		if current_user_name:
+			role=check_role(current_user_name)
+			if 'admin' in role:
+				#ADMIN
+				if request.method == 'PUT':
+					try:
+						title = str(request.form['title'])
+						desc=str(request.form['desc'])
+						stock=str(request.form['stock'])
+						desc=str(request.form['desc'])
+						image=str(request.form['image'])
+						rent_price=float(request.form['rent_price'])
+						sale_price=float(request.form['sale_price'])
+						availability=int(request.form['availability'])
+						#insert de nuevos valores
+						conn = sqlite3.connect("movies.db")
+						cursor = conn.cursor()
+						cursor.execute("UPDATE movies SET title=?, description=?,image=?,stock=?, rent_price=?,sale_price=?, availability=? WHERE id_movie=?",(title,desc,image,stock,rent_price,sale_price,availability,str(key)))
+						conn.commit()
+						logger.info(str(title)+" has been updated, rental price: $"+str(rent_price)+", sale price: $"+str(rent_price))
+						return jsonify(status="success",current_user=current_user_name,rol=role), 200
+					except Exception as e:
+						print(e)
+						return jsonify(status="error"),
+					conn.close()
+
+				#ADMIN
+				if request.method == 'DELETE':
+					try:
+						movie=[]
+						conn = sqlite3.connect("movies.db")
+						conn.execute("DELETE FROM movies WHERE id_movie="+str(key))
+						conn.commit()
+						conn.close()
+						logger.info("Movie "+str(key)+" has been deleted")
+						return jsonify(status="success",current_user=current_user_name,rol=role), 201
+					except Exception as e:
+						print(e)
+						return jsonify(status="error"),
+			else:	
+				return jsonify(status="error, you're not allowed, you're not admin",current_user=current_user_name,rol=role), 200
+		else:	
+			return jsonify(status="error, you're not allowed, you're a visitor"), 200
 
 
 @app.route('/login', methods=['POST'])
@@ -314,19 +323,6 @@ def logout():
     unset_jwt_cookies(resp)
     return resp, 200
     
-    
-# Protect a view with jwt_required, which requires a valid access token
-# in the request to access.
-# @app.route('/protected', methods=['GET'])
-# @jwt_required
-# def protected():
-    # # Access the identity of the current user with get_jwt_identity
-    # current_user = get_jwt_identity()
-    # if current_user:
-        # return jsonify(logged_in_as=current_user), 200
-    # else:
-        # return jsonify(logged_in_as='anonymous user'), 200
-    # return jsonify(logged_in_as=current_user), 200
     
 def check_role(user_name):
 	conn = sqlite3.connect("movies.db")
